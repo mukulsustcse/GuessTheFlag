120227a9e332ad1599bbc39c436b2932778f8d98
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-20 16:44:51 +0200

    Working - change the flag when tap on the flag button - show a pop up screen showing the right or wrong answer

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -14,15 +14,19 @@ struct ContentView: View {
     ].shuffled()
     
     @State private var showingScore = false
+    @State private var scoreTitle = ""
     
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         
         ZStack {
-            Color.blue
-                .ignoresSafeArea()
-            Color.secondary
+//            Color.blue
+//                .ignoresSafeArea()
+//            Color.secondary
+//                .ignoresSafeArea()
+            
+            LinearGradient(gradient: Gradient(colors: [.black, .blue]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
             VStack(spacing: 30) {
@@ -30,9 +34,11 @@ struct ContentView: View {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
                     Text(countries[correctAnswer])
                         .bold()
                         .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
                 
                 ForEach(0..<3) {
@@ -41,15 +47,42 @@ struct ContentView: View {
                     
                     Button(action: {
                         
+                        flagTapped(number)
+                        
                     }, label: {
                         Image(countries[number])
                             .renderingMode(.original)
+                            .cornerRadius(15)
+                            .shadow(radius: 3, x: 12, y: 10)
                     })
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
         
+    }
+    
+    func flagTapped(_ number:Int) {
         
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        }
+        else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
