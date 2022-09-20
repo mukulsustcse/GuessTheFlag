4534ded1d5122864bf055264ac777a7ffd42498a
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-20 17:28:47 +0200

    Update Score

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var answerMessage = ""
     
     @State private var correctAnswer = Int.random(in: 0...2)
     
@@ -29,40 +31,71 @@ struct ContentView: View {
             LinearGradient(gradient: Gradient(colors: [.black, .blue]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
-            VStack(spacing: 30) {
+//            RadialGradient(stops: [
+//                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+//                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+//            ], center: .top, startRadius: 200, endRadius: 700)
+//            .ignoresSafeArea()
+            
+            VStack {
                 
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                        .font(.subheadline.weight(.heavy))
-                    Text(countries[correctAnswer])
-                        .bold()
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
+                Spacer()
+                Spacer()
                 
-                ForEach(0..<3) {
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
                     
-                    number in
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .bold()
+//                            .foregroundColor(.black)
+                            .font(.largeTitle.weight(.semibold))
+                    }
                     
-                    Button(action: {
+                    ForEach(0..<3) {
                         
-                        flagTapped(number)
+                        number in
                         
-                    }, label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
-                            .cornerRadius(15)
-                            .shadow(radius: 3, x: 12, y: 10)
-                    })
+                        Button(action: {
+                            
+                            flagTapped(number)
+                            
+                        }, label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .cornerRadius(15)
+                                .shadow(radius: 3, x: 12, y: 10)
+                        })
+                    }
                 }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                Spacer()
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             
             Button("Continue", action: askQuestion)
+            
         } message: {
-            Text("Your score is ???")
+            Text(answerMessage)
         }
         
     }
@@ -71,9 +104,17 @@ struct ContentView: View {
         
         if number == correctAnswer {
             scoreTitle = "Correct"
+            
+            score += 100
+            
+            answerMessage  = "Yahoo! Your answer is Correct. You have earned 100 points."
         }
         else {
             scoreTitle = "Wrong"
+            
+            score -= 50
+            
+            answerMessage  = "Oops! Your answer is Wrong. You have lost 50 points."
         }
         
         showingScore = true
