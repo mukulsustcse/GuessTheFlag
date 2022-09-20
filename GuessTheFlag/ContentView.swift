fca2e25a7a8b1326cc2131368ec545ef5bbc960e
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-20 16:09:40 +0200

    basic design and structure is ready

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var countries = [
+        "Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"
+    ].shuffled()
+    
+    @State private var showingScore = false
+    
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            Color.secondary
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0..<3) {
+                    
+                    number in
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    })
+                }
+            }
+        }
+        
+        
     }
 }
 
